d0f48adc7885a3176060567f3da2a63c29e93395
Angel Rios <riosmartinezkg@gmail.com>
2025-07-06 16:11:50 -0600

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";    
+
+export default defineConfig({
+    base: '/js-vite-blackjack/',
+}); // This is a basic Vite configuration file. You can add plugins and other configurations as needed.  
